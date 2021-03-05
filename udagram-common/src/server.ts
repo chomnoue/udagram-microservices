2bbc778f5a52c339cd4d939a5a78cc82be8bac88
Chomnoue Nghemning Alain-Michel <chomnoue@yahoo.fr>
2021-03-05 13:45:47 +0800

    add loggs for every requests

--- udagram-common/src/server.ts
+++ udagram-common/src/server.ts
@@ -1,12 +1,33 @@
 import cors from 'cors';
-import express, {Router} from 'express';
+import express, {Request, Response, Router} from 'express';
 import {getSequelize} from './sequelize';
 
 import bodyParser from 'body-parser';
 import {DBConfig} from './config';
 import {Model} from 'sequelize-typescript';
 import {Server} from "http";
+import morgan from 'morgan';
+import { v4 as uuidv4 } from 'uuid';
+import {NextFunction} from "connect";
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      id: string;
+    }
+  }
+}
+
+function assignId (req: Request, res: Response, next: NextFunction): void {
+  const idHeader = req.header('X-Request-Id');
+  if (idHeader) {
+    req.id = idHeader.toString();
+  } else {
+    req.id = uuidv4();
+  }
+  next()
+}
 
 export async function startServer<M extends typeof Model> (config: DBConfig, models: M[], url: string, router: Router):
     Promise<Server> {
@@ -17,6 +38,12 @@ export async function startServer<M extends typeof Model> (config: DBConfig, mod
   const app = express();
   const port = process.env.PORT || 8080;
 
+  morgan.token('id', function getRequestId (req: Request) {
+    return req.id
+  })
+
+  app.use(assignId);
+  app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] - :id - :response-time'));
   app.use(bodyParser.json());
 
   app.use(cors({
